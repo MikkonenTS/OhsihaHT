a15bde5a10f26b9ef51e8a4cf0b879f3e62d1142
MikkonenTS <mikkonen.ts@gmail.com>
2018-02-23 09:48:30 +0200

    testi

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Teme\GOOGLE~1\KOULUH~1\TITE\OHSIHA~1\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ login/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+#class Profiles(object):
+#    """Käyttäjäprofiilien tallennus"""
+#    username = models.OneToOneField(USER)
+#    email = models.CharField(max_length=50)
+#   firstname = models.CharField(max_length=50)
+#   lastname = models.CharField(max_length=50)
+
+
+class Palautus(models.Model):
+    palauttaja = models.CharField(max_length=200)
+    palautuspvm = models.DateTimeField('korvauksen pvm')
+    #palautustiedosto
+
+#MUISTA, DJANGO LUO ID:N AUTOMAATTISESTI
+# yläpuolella polls appin koodia
+# Create your models here.
