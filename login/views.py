5f76a2eceaeccb94e34a55a645e9326cb3b1deb7
MikkonenTS <mikkonen.ts@gmail.com>
2018-02-24 23:29:55 +0200

    Lisätty path Signup-osioon

--- login/views.py
+++ login/views.py
@@ -1,8 +1,12 @@
 from django.http import HttpResponse
-#from django.shortcuts import render
+from django.shortcuts import render
 
 def index(request):
 
-    return HttpResponse("Tervetuloa N.N-killan kulukorvausjärjestelmään")
-#def home(request):
-#    return render(request, 'home.html', {})
+    return HttpResponse("Tervetuloa, kirjaudu sisään tai rekisteröidy"),
+
+def signup(request):
+
+    #if request.methon = 'POST':
+
+    return render(request,'signup.html')
